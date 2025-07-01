fdd8d7abece6505313ec1f8c858ba2a398bae36a
ETRI 김민준 <pollo1999@etri.re.kr>
2025-07-02 03:27:14 +0900

    use suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
+import {Suspense} from "react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex items-center justify-center`}
       >
-        {children}
+        <Suspense fallback={<div>Loading page...</div>}>{children}</Suspense>
       </body>
     </html>
   );
