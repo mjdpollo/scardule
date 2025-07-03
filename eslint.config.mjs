e079767360dd0f57a5e972a5a1d17e17df039b13
mjthepollo <mjthepollo@gmail.com>
2025-07-03 23:55:50 +0900

    change eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import {FlatCompat} from "@eslint/eslintrc";
+import pluginReact from "eslint-plugin-react";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // Enforce arrow function components by default
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      "react/function-component-definition": [
+        "error",
+        {
+          namedComponents: "function-declaration",
+          unnamedComponents: "function-expression", // for HOC returns
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
